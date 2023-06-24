53fd2e9faa98cd28fc9980fac721225401905c0f
CarlosVaras <CarlosVaras@users.noreply.github.com>
2023-06-24 23:11:01 +0000

    modified app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+""" streamlit container-like for both backend and frontend
+    pandas for data access
+    pygwalker for a Tableau-like UI
+"""
 import streamlit as st
 import pandas as pd
 import pygwalker as pyg
@@ -10,20 +14,20 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Load Data
 @st.cache_data
 def load_data(url):
-    df = pd.read_csv(url)
-    return df
+    """ load_data returns dataframe using URL param """
+    dataframe = pd.read_csv(url)
+    return dataframe
 df = load_data("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
 
 # Set title and subtitle
 st.title('PyGWalker Demo App')
 st.subheader('A demonstration of the PyGWalker Python library')
 
-# Display PyGWalker
 def load_config(file_path):
-    with open(file_path, 'r') as config_file:
+    """ load_config displays PyGWalker UI """
+    with open(file_path, 'r', encoding='UTF-8') as config_file:
         config_str = config_file.read()
     return config_str
 config = load_config('config.json')
